27a54b7f0790902418d66778d558f2929153a4a3
Mark Bradshaw <notthehoople@googlemail.com>
2020-05-02 15:59:20 +0100

    Added random tweak to ball direction after collision
    Attempt to stop the dull back and forth across the screen we sometimes get

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
@@ -9,6 +7,7 @@ public class Ball : MonoBehaviour
     [SerializeField] float xPush = 2f;
     [SerializeField] float yPush = 10f;
     [SerializeField] AudioClip[] ballSounds;
+    [SerializeField] float randomAngleFactor = 0.2f;
 
     // State
     Vector2 paddleToBallVector;
@@ -16,12 +15,14 @@ public class Ball : MonoBehaviour
 
     // Cached component references
     AudioSource myAudioSource;
+    Rigidbody2D myRigidBody;
 
     // Start is called before the first frame update
     void Start()
     {
         paddleToBallVector = transform.position - paddle1.transform.position;
         myAudioSource = GetComponent<AudioSource>();
+        myRigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -39,7 +40,7 @@ public class Ball : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             hasStarted = true;
-            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+            myRigidBody.velocity = new Vector2(xPush, yPush);
         }
     }
 
@@ -52,10 +53,13 @@ public class Ball : MonoBehaviour
     // Runs every time there's a collision
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        Vector2 velocityTweak = new Vector2(Random.Range(0f,randomAngleFactor), Random.Range(0f,randomAngleFactor));
+
         if (hasStarted)
         {
             AudioClip clip = ballSounds[Random.Range(0, ballSounds.Length)];
             myAudioSource.PlayOneShot(clip);
+            myRigidBody.velocity += velocityTweak;
         }
     }
 }
